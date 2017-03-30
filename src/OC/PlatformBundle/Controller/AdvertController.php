65e355244b684a36406e93a415bd5fb988597601
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2017-03-30 09:29:58 +0200

    delete default Bundle and dependencies

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maximebriand
+ * Date: 30/03/2017
+ * Time: 09:00
+ */
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdvertController extends Controller
+{
+    public function indexAction(){
+        $content = $this
+            ->get('templating')
+            ->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzo'));
+        return new Response($content);
+    }
+}
